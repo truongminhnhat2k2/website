a8543a680465dc04b643844f64356eeaadc5f846
Admin <Admin@LAPTOPCUAHIEU>
2023-09-12 19:51:40 +0700

    Add project files.

---
+++ Website/Controllers/CourseSingleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Website.Controllers
+{
+    public class CourseSingleController : Controller
+    {
+        // GET: CourseSingle
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
